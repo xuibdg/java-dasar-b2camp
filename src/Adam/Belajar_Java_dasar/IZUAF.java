1fc771a4bf4e11b4b6bde048dcbb0d25922ba706
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-18 23:02:36 +0700

    IZUAF selesai

--- src/Adam/Belajar_Java_dasar/IZUAF.java
+++ src/Adam/Belajar_Java_dasar/IZUAF.java
@@ -1,4 +1,9 @@
 package Adam.Belajar_Java_dasar;
 
 public class IZUAF {
+    public static void main(String[] args) {
+        String a = "Muhammad Adam Majid Aditama";
+        String reversed = new StringBuilder(a).reverse().toString();
+        System.out.println(reversed);
+            }
 }
