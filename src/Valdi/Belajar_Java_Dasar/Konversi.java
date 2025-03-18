0b6336878eaeb0680e231942b135f8f2cfdb58f8
Rivaldi Surya <29rivaldisuryasaputra@gmail.com>
2025-03-18 22:22:39 +0700

    menambahkan tugas no4 tentang konversi int ke double

---
+++ src/Valdi/Belajar_Java_Dasar/Konversi.java
@@ -0,0 +1,13 @@
+package Valdi.Belajar_Java_Dasar;
+
+public class Konversi {
+    public static void main(String[] args) {
+        int AngkaInt = 29;  // Nilai awal dalam tipe int
+        double AngkaDouble = AngkaInt; // Konversi otomatis ke double
+
+        System.out.println("Nilai int     : " + AngkaInt);
+        System.out.println("Setelah jadi double: " + AngkaDouble);
+        //int itu bilangan bulat tanpa decimal
+        //double itu bilangan decimal
+    }
+}
