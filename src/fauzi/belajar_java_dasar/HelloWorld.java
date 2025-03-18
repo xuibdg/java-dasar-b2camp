6bd767b53758f0ebcabec2af2bc91337fe3304f7
denzi <fauziindrawan111@gmail.com>
2025-03-18 14:42:00 +0700

    first commit

---
+++ src/fauzi/belajar_java_dasar/HelloWorld.java
@@ -0,0 +1,7 @@
+package fauzi.belajar_java_dasar;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/fauzi/belajar_java_dasar/Variable.java
@@ -0,0 +1,28 @@
+package fauzi.belajar_java_dasar;
+
+public class Variable {
+    public static void main(String[] args) {
+        int age = 25;
+        String name = "farhan";
+        Boolean isActive = true ;
+        boolean isDeleted = false;
+
+        //typeData namaVariable = value
+        int a = 10;
+        int b = 20;
+        int c = a + b ;
+
+
+        System.out.println(isDeleted);
+        System.out.println(isActive);
+        System.out.println("name : " + name);
+        System.out.println("age : " + age);
+        System.out.println("isActive : " + isActive);
+
+        System.out.println("nilai akhir : " + c);
+        System.out.println("name : " + name + " age : " + age + " isActive : " + isActive);
+
+    }
+
+}
+
