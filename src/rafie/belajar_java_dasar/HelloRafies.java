6e11784d62a9299b740d68222990f7abfbd08e37
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-03-18 19:58:36 +0700

    first commit

---
+++ src/rafie/belajar_java_dasar/HelloRafies.java
@@ -0,0 +1,9 @@
+package rafie.belajar_java_dasar;
+
+public class HelloRafies {
+    public static void main(String[] args) {
+        System.out.println("Hello Rafie, this is your assistant");
+        System.out.print("Yes, assistant. I am starving, ");
+        System.out.print("give me food please");
+    }
+}
