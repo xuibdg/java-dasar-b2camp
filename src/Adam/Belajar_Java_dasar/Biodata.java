18b079fb14fcbb20d3c90fe90a0a77ad28bb9b31
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-18 23:00:17 +0700

    Biodata selesai

---
+++ src/Adam/Belajar_Java_dasar/Biodata.java
@@ -0,0 +1,19 @@
+package Adam.Belajar_Java_dasar;
+
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "adam";
+        int umur = 17;
+        double tinggiBadan = 172;
+        char jenisKelamin = 'P';
+        boolean sudahMenikah = false;
+        String alamat = "Lampung Timur";
+        System.out.println("name : " + nama);
+        System.out.println("umur :" + umur);
+        System.out.println("tinggiBadan :" + tinggiBadan);
+        System.out.println("jenisKelamin :" + jenisKelamin);
+        System.out.println("sudahMenikah :" + sudahMenikah);
+        System.out.println("alamat :" + alamat);
+    }
+}
+
