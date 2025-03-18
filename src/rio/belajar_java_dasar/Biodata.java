3ec3c395c73f9455779d04bcfd8f3a7be543d09f
rio <ainktz3089@gmail.com>
2025-03-19 01:30:13 +0700

    menambahkan biodata

---
+++ src/rio/belajar_java_dasar/Biodata.java
@@ -0,0 +1,18 @@
+package rio.belajar_java_dasar;
+
+public class Biodata {
+    public static void main(String[] args) {
+        String Nama = "Rio";
+        int Umur = 34;
+        double TinggiBadan = 165;
+        char JenisKelamin = 'L';
+        boolean SudahMenikah = true;
+        String Alamat = "Ujungberung, Bandung";
+
+        System.out.println("Nama : " + Nama);
+        System.out.println("Umur : " + Umur);
+        System.out.println("Tinggi Badan : " + TinggiBadan);
+        System.out.println("Sudah Menikah : " + SudahMenikah);
+        System.out.println("Alamat : " + Alamat);
+    }
+}
