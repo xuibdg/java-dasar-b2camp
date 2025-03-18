45110da742b85388376b52eadc5bf7a1b9e14218
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-18 23:04:48 +0700

    Konversi selesai

--- src/Adam/Belajar_Java_dasar/Konversi.java
+++ src/Adam/Belajar_Java_dasar/Konversi.java
@@ -1,4 +1,25 @@
 package Adam.Belajar_Java_dasar;
 
 public class Konversi {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        int c = 1;
+        double x = a;
+        double y = b;
+        double z = c;
+        System.out.println("Int to double 4 = " + x);
+        System.out.println("Int to double 3 = " + y);
+        System.out.println("Int to double 2 = " + z);
+
+        double r = 4.00;
+        double s = 3.00;
+        double t = 2.00;
+        int d = (int) r;
+        int e = (int) s;
+        int f = (int) t;
+        System.out.println("Double to int 4.00 = " + d);
+        System.out.println("Double to int 3.00 = " + e);
+        System.out.println("Double to int 2.00 = " + f);
+    }
 }
