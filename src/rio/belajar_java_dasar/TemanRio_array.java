968caaa6b131250ce7d0cc0bafbf746c3882dce2
rio <ainktz3089@gmail.com>
2025-03-19 01:15:46 +0700

    menambahkan tugas, konversi tipe data, reverse name, array

---
+++ src/rio/belajar_java_dasar/TemanRio_array.java
@@ -0,0 +1,8 @@
+package rio.belajar_java_dasar;
+
+public class TemanRio_array {
+    public static void main(String[] args) {
+        String[] teman = {"Abi", "Dudi", "Mia", "Anwar", "Elly"};
+        System.out.println("Nama di indeks ke-3: " + teman[3]);
+    }
+}
