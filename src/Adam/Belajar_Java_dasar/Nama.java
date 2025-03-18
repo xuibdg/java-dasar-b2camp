b724470fe81045abc7b064442993bdec13125a30
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-18 23:03:38 +0700

    Nama-nama selesai

--- src/Adam/Belajar_Java_dasar/Nama.java
+++ src/Adam/Belajar_Java_dasar/Nama.java
@@ -1,4 +1,11 @@
 package Adam.Belajar_Java_dasar;
 
 public class Nama {
+    public static void main(String[] args) {
+        String[] nama = {"Adam", "Jauhari", "Ikhsan", "Rio", "Faldi"};
+        System.out.println(nama[3]);
+    }
+
+
+
 }
