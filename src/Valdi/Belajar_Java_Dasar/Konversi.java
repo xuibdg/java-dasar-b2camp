2832d0c0d70459979bd44709f9ee29deb8396c21
Rivaldi Surya <29rivaldisuryasaputra@gmail.com>
2025-03-19 10:38:36 +0700

    menjelaskan tentang no4

--- src/Valdi/Belajar_Java_Dasar/Konversi.java
+++ src/Valdi/Belajar_Java_Dasar/Konversi.java
@@ -3,13 +3,16 @@ package Valdi.Belajar_Java_Dasar;
 public class Konversi {
     public static void main(String[] args) {
 
-        int angkaInt = 2929;
-        double angkaDouble = (double) angkaInt;
+        int AngkaInt = 2929;
+        double AngkaDouble = (double) AngkaInt;
+        //konversi dilakukan oleh "(double) angkaInt"
 
-        double angkaDouble2 = 12.29;
-        int angkaInt2 = (int) angkaDouble2;
 
-        System.out.println("Konversi int ke double: " + angkaDouble);
-        System.out.println("Konversi double ke int: " + angkaInt2);
+        double AngkaDouble2 = 12.29;
+        int AngkaInt2 = (int) AngkaDouble2;
+        //konversi dilakukan oleh "(int) angkaDouble2"
+
+        System.out.println("Konversi int ke double: " + AngkaDouble);
+        System.out.println("Konversi double ke int: " + AngkaInt2);
     }
 }
