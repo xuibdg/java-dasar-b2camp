5afa0ae1d9a07cb909900c7de05ee7e80139835d
Ikam Aja <macbookpro@MacBook-Pro-MacBook-ikam.local>
2025-03-20 04:42:50 +0700

    merapihkan codingan

--- src/Ichsan/belajar_java_Dasar/usia.java
+++ src/Ichsan/belajar_java_Dasar/usia.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class usia {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Masukan Usia: ");
-     int usia =scanner.nextInt();
-     String  kumpulanUsia=(usia < 13) ? "Bocil": (usia <= 17) ? "Remaja" : (usia <= 59) ? "Dewasa":
-             (usia <= 60) ? "Aki-Aki" : "lansia";
-        System.out.println("Kategori Usia: " + kumpulanUsia );
+        int usia = scanner.nextInt();
+        String kumpulanUsia =
+                (usia < 13) ? "Bocil" :
+                        (usia <= 17) ? "Remaja" :
+                        (usia <= 59) ? "Dewasa" :
+                        (usia <= 60) ? "Aki-Aki" : "lansia";
+        System.out.println("Kategori Usia: " + kumpulanUsia);
         scanner.close();
     }
 }
