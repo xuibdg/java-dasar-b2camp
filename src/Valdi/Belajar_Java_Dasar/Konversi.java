fd08a82927199648ac0b32b7bf6bebe624ff89a7
Rivaldi Surya <29rivaldisuryasaputra@gmail.com>
2025-03-19 10:33:17 +0700

    mengkoreksi soal no4 tentang konversi

--- src/Valdi/Belajar_Java_Dasar/Konversi.java
+++ src/Valdi/Belajar_Java_Dasar/Konversi.java
@@ -2,12 +2,14 @@ package Valdi.Belajar_Java_Dasar;
 
 public class Konversi {
     public static void main(String[] args) {
-        int AngkaInt = 29;  // Nilai awal dalam tipe int
-        double AngkaDouble = AngkaInt; // Konversi otomatis ke double
 
-        System.out.println("Nilai int     : " + AngkaInt);
-        System.out.println("Setelah jadi double: " + AngkaDouble);
-        //int itu bilangan bulat tanpa decimal
-        //double itu bilangan decimal
+        int angkaInt = 2929;
+        double angkaDouble = (double) angkaInt;
+
+        double angkaDouble2 = 12.29;
+        int angkaInt2 = (int) angkaDouble2;
+
+        System.out.println("Konversi int ke double: " + angkaDouble);
+        System.out.println("Konversi double ke int: " + angkaInt2);
     }
 }
