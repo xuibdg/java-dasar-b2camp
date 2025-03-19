44f5294146326a393c54d84837c445197a3700da
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-20 00:31:24 +0700

    no 3 selesai (ini nomer 2, yang sebelumnya no 3)

--- src/Adam/Belajar_Java_dasar/UsiadanKategori.java
+++ src/Adam/Belajar_Java_dasar/UsiadanKategori.java
@@ -1,4 +1,19 @@
 package Adam.Belajar_Java_dasar;
 
+import java.util.Scanner;
+
 public class UsiadanKategori {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("USIA DAN KATEGORI");
+
+        int usia = scanner.nextInt();
+
+        String kategori = (usia <= 13) ? "Anak-anak" : (usia <= 17) ? "Remaja" :
+                (usia <= 59) ? "Dewasa" : "Lansia";
+        System.out.println("Kategori : " + kategori);
+
+
+        scanner.close();
+    }
 }
