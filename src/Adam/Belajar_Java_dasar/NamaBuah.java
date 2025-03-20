7fce5b298ea153fd252365c1dc873ae48059903c
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-21 06:50:03 +0700

    NamaBuah

--- src/Adam/Belajar_Java_dasar/NamaBuah.java
+++ src/Adam/Belajar_Java_dasar/NamaBuah.java
@@ -1,4 +1,21 @@
 package Adam.Belajar_Java_dasar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NamaBuah {
+    public static void main(String[] args) {
+        System.out.println("NAMA-NAMA BUAH YANG ENAK DIJADIKAN BUKA PUASA");
+        List<String> list = new ArrayList<>();
+        list.add("KURMA");
+        list.add("SEMANGKA");
+        list.add("APEL");
+        list.add("MANGGA");
+        list.add("ANGGUR");
+        list.add("MELON");
+        list.add("NAGA");
+        list.add("PIR");
+        list.remove("MANGGA");
+        System.out.println(list);
+    }
 }
