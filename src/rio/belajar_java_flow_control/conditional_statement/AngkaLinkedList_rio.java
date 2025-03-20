42a2d917b4dbb20d5e8254ad569606d17dcf9b46
rio <ainktz3089@gmail.com>
2025-03-20 22:09:42 +0700

    menambahkan tugas ganjil-genap, daftar buah, angka

--- src/rio/belajar_java_flow_control/conditional_statement/AngkaLinkedList_rio.java
+++ src/rio/belajar_java_flow_control/conditional_statement/AngkaLinkedList_rio.java
@@ -5,12 +5,17 @@ import java.util.LinkedList;
 public class AngkaLinkedList_rio {
     public static void main(String[] args) {
         LinkedList<String> AngkaLinkedlist = new LinkedList<>();
+        //sebuah wadah/tempat untuk menyimpan berupa teks/string yg lebih fleksibel & efisien
+
         AngkaLinkedlist.add("11");
         AngkaLinkedlist.add("13");
         AngkaLinkedlist.add("14");
         AngkaLinkedlist.add("15");
+        //AngkaLinkedlist.addFirst("12");
 
         AngkaLinkedlist.addLast("12");  //menjadi elemen/urutan terakhir
         System.out.println(AngkaLinkedlist);
     }
 }
+// 4. Buatlah program yang menyimpan beberapa angka dalam LinkedList
+// lalu mencetak elemen pertama dan terakhir
--- src/rio/belajar_java_flow_control/conditional_statement/DaftarBuah_rio.java
+++ src/rio/belajar_java_flow_control/conditional_statement/DaftarBuah_rio.java
@@ -1,10 +1,13 @@
 package rio.belajar_java_flow_control.conditional_statement;
 
 import java.util.ArrayList;
-
+// 2. Buatlah program yang menyimpan daftar nama buah dalam ArrayList
+// lalu mencetak semua elemen di dalamnya
 public class DaftarBuah_rio {
     public static void main(String[] args) {
         ArrayList<String> buahList = new ArrayList<>();
+        //sebuah wadah/tempat untuk menyimpan data yg berupa teks/string
+        //dgn akses elemen yg dinamis (bisa bertambah atau dikurngi)
 
         buahList.add("Anggur");
         buahList.add("Apel");
