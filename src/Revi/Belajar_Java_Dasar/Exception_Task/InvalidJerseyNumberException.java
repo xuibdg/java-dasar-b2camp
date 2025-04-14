32de87713588e6e7bbd9f8035cf0b09da23288d6
Revisatr123 <revisatria123@gmail.com>
2025-04-15 03:32:15 +0700

    Exception Task

---
+++ src/Revi/Belajar_Java_Dasar/Exception_Task/InvalidJerseyNumberException.java
@@ -0,0 +1,24 @@
+package Revi.Belajar_Java_Dasar.Exception_Task;
+
+public class InvalidJerseyNumberException extends Exception {
+    public InvalidJerseyNumberException(String message) {
+        super(message);
+        class CustomExceptionExample {
+
+            public static void cekNomor(int nomor) throws InvalidJerseyNumberException {
+                if (nomor < 1 || nomor > 99) {
+                    throw new InvalidJerseyNumberException("Nomor tidak valid: " + nomor);
+                }
+                System.out.println("Nomor valid: " + nomor);
+            }
+
+            public static void main(String[] args) {
+                try {
+                    cekNomor(10);   // valid
+                    cekNomor(0);    // tidak valid
+                } catch (InvalidJerseyNumberException e) {
+                    System.out.println("Exception tertangkap: " + e.getMessage());
+                }
+            }
+        }}
+}
