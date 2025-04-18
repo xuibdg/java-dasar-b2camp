732b1f63de95f4a8de4bb1d6a9ec6f86572034b0
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-04-18 08:11:16 +0700

    done: Task14April.java

--- src/rafie/belajar_java_dasar/Task_14_April_2025/Task14April.java
+++ src/rafie/belajar_java_dasar/Task_14_April_2025/Task14April.java
@@ -1,6 +1,36 @@
 package rafie.belajar_java_dasar.Task_14_April_2025;
 
-public class Task14April {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Task14April extends Exception{
+    public Task14April(String message) {
+        super(message);
+    }
+}
+
+class LicensePlateException {
+    public static void main(String[] args) throws Task14April {
+        System.out.println("Selamat datang di Program Cek Plat Nomor Kendaraan");
+        Scanner inputanUser = new Scanner(System.in);
+
+        try{
+            System.out.print("Silahkan masukan huruf pertama plat nomor kendaraan Anda: ");
+            String inputanPlat = inputanUser.next();
+            validatePlat(inputanPlat);
+        } catch (Task14April e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Program Cek Plat Nomor Kendaraan, telah selesai.");
+            inputanUser.close();
+
+        }
+    }
+
+    public static void validatePlat(String plat) throws Task14April {
+        if (plat.equals("D")) {
+            throw new Task14April("Kendaraan Anda terdata di Jawa Barat");
+        } else {
+            throw new Task14April("Kendaraan Anda terdata diluar Jawa Barat");
+        }
     }
 }
