a7422ea485428b4c58fdbf1b41af01885868ec28
rio <ainktz3089@gmail.com>
2025-04-24 00:44:21 +0700

    task tgl 23 april 2025, java i/o

---
+++ src/rio/javaio_task_23_apr/FileReaderRio.java
@@ -0,0 +1,23 @@
+package rio.javaio_task_23_apr;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderRio {
+    public static void main(String[] args) {
+        String filePath = "mulDim.xlsx";
+        int number = 100;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while ((line = reader.readLine()) != null){
+                System.out.println(line.replace(",", " "));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+        System.gc();
+    }
+}
---
+++ src/rio/javaio_task_23_apr/FileWriterRio.java
@@ -0,0 +1,46 @@
+package rio.javaio_task_23_apr;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class FileWriterRio {
+    public static void main(String[] args) {
+        String filePath = "mulDim.xlsx";
+
+        String muldimFinally =
+                "o,o,o,o, , ,o, , ,o,o,o, , , ,o,o,o,o, , ,o, ,o, , , ,o, , ,o,o,o, , ,o, , , , , ,o, , ,o,o,o, ,;," +
+                "o, , , ,o, ,o, ,o, , , ,o, , ,o, , , ,o, ,o, ,o, , ,o, , ,o, , , ,o, ,o, , , , , ,o, ,o, , , ,o,;," +
+                "o, , , ,o, ,o, ,o, , , ,o, , ,o, , , ,o, ,o, ,o, ,o, , , ,o, , , ,o, ,o,o, , , , ,o, ,o, , , ,o,;," +
+                "o, , , ,o, ,o, ,o, , , ,o, , ,o, , , ,o, ,o, ,o,o, , , , ,o, , , ,o, ,o, ,o, , , ,o, ,o, , , ,o,;," +
+                "o,o,o,o, , ,o, ,o, , , ,o, , ,o,o,o,o, , ,o, ,o, , , , , ,o,o,o,o,o, ,o, , ,o, , ,o, ,o,o,o,o,o,;," +
+                "o,o, , , , ,o, ,o, , , ,o, , ,o,o, , , , ,o, ,o,o, , , , ,o, , , ,o, ,o, , , ,o, ,o, ,o, , , ,o,;," +
+                "o, ,o, , , ,o, ,o, , , ,o, , ,o, ,o, , , ,o, ,o, ,o, , , ,o, , , ,o, ,o, , , , ,o,o, ,o, , , ,o,;," +
+                "o, , ,o, , ,o, ,o, , , ,o, , ,o, , ,o, , ,o, ,o, , ,o, , ,o, , , ,o, ,o, , , , , ,o, ,o, , , ,o,;," +
+                "o, , , ,o, ,o, , ,o,o,o, , , ,o, , , ,o, ,o, ,o, , , ,o, ,o, , , ,o, ,o, , , , , ,o, ,o, , , ,o,;,";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))){
+            String[] Split = muldimFinally.split(",");
+            Arrays.stream(Split).forEach(data -> {
+                if (!data.equals(";")){
+                    try {
+                        writer.write(data + ",");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else {
+                    try {
+                        writer.newLine();
+                    } catch (IOException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            System.out.println("Data berhasil disimpan ke file");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.gc();
+    }
+}
