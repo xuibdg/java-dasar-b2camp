047fec1c1b635dfd2be543eeab08d5c8e55eeb65
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-04-23 12:09:51 +0700

    belajar ketertinggalan materi solid

---
+++ src/rafie/belajar_java_dasar/solid_learn/StockProduct.java
@@ -0,0 +1,15 @@
+package rafie.belajar_java_dasar.solid_learn;
+
+public class StockProduct implements Operation {
+    private
+
+    @Override
+    public void stockProduct(int stock) {
+        qty += stock;
+    }
+
+    @Override
+    public void transaction(double amount) {
+
+    }
+}
