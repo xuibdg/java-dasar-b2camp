a2af9f011f1ef7efcaef1f8df039168337de2045
hirlan <landthursmorn@gmail.com>
2025-05-15 10:18:14 +0700

    print name

---
+++ src/hirlan/javaio/patern/MainAlphabet.java
@@ -0,0 +1,12 @@
+package hirlan.javaio.patern;
+
+public class MainAlphabet {
+    public static void main(String[] args) {
+        AlphabetExample examples = new AlphabetExample();
+        examples.main(args);
+
+        System.out.println();
+        System.out.println("Running di class Baru");
+    }
+
+}
